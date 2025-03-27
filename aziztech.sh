e6d33d2580b3b2357abc5e2440e487942167874b
azizcool1998 <azizcool1998@gmail.com>
2025-03-28 02:29:37 +0800

    aziztech.sh

--- aziztech.sh
+++ aziztech.sh
@@ -231,7 +231,7 @@ elif [ "$SELECT_THEME" -eq 3 ]; then
   sudo mkdir -p /etc/apt/keyrings
   curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
   echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-  y
+  
 
 # Lanjutan
   cd
