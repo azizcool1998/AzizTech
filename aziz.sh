ec620d88628c003be02e2a355641b019fb786a7c
azizcool1998 <azizcool1998@gmail.com>
2025-04-03 18:20:22 +0800

    aziz.sh

--- aziz.sh
+++ aziz.sh
@@ -62,7 +62,7 @@ check_token() {
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
   read -r USER_TOKEN
 
-  if [ "$USER_TOKEN" = "aziztech" ]; then
+  if [ "$USER_TOKEN" = "q" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
     echo -e "${RED}Token Salah! Beli Kode Token Di AzizTech${NC}"
